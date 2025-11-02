16727db3fd67d7abc0241092babee1633accab1d
unknown <alexandru.marian99777@gmail.com>
2025-11-02 16:23:08 +0200

    second commit

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 // Connect to MongoDB using URI from .env
 mongoose.connect(process.env.MONGO_URI, {
   
